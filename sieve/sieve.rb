718e295bac02baf5d1cc42f704dc005e690ebe7a
emily.c.ritter <emily.c.ritter@gmail.com>
2016-06-13 23:11:37 -0500

    Sieve - clean up methods

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -1,21 +1,18 @@
 class Sieve
   def initialize (num)
-    @num = num
+    if num > 1
+      @array_of_num = (2..num).to_a
+    else
+      @array_of_num = []
+    end
   end
 
   def primes
-    if @num > 1
-      array_of_num = (2..@num).to_a
-      array_of_primes = array_of_num
-      array_of_num.each do |num|
-        array_of_primes.each do |potential_prime|
-          if potential_prime%num == 0 && potential_prime != num
-            array_of_primes.delete(potential_prime)
-          end
-        end
+    array_of_primes = @array_of_num
+    @array_of_num.each do |num|
+      array_of_primes.each do |potential_prime|
+        array_of_primes.delete(potential_prime) if potential_prime%num == 0 && potential_prime != num
       end
-    else
-      array_of_primes = []
     end
     return array_of_primes
   end
