78da56b87ef0b11c6e0a2bd9bfb66fc2eb9557e2
emily.c.ritter <emily.c.ritter@gmail.com>
2016-06-16 08:19:37 -0500

    Roman Numerals - move hashes into a single method

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -14,27 +14,29 @@ class Fixnum
   end
 
   def ones_digit (roman_numeral, num)
-    my_digit_hash = {0 => '', 1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV', 5 => 'V', 6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX'}
-    calculate_digit(roman_numeral, num, my_digit_hash, -1)
+    calculate_digit(roman_numeral, num, -1)
   end
 
   def tens_digit (roman_numeral, num)
-    my_digit_hash = {1 => 'X', 2 => 'XX', 3 => 'XXX', 4 => 'XL', 5 => 'L', 6 => 'LX', 7 => 'LXX', 8 => 'LXXX', 9 => 'XC'}
-    calculate_digit(roman_numeral, num, my_digit_hash, -2)
+    calculate_digit(roman_numeral, num, -2)
   end
 
   def hundreds_digit (roman_numeral, num)
-    my_digit_hash = {1 => 'C', 2 => 'CC', 3 => 'CCC', 4 => 'CD', 5 => 'D', 6 => 'DC', 7 => 'DCC', 8 => 'DCCC', 9 => 'CM'}
-    calculate_digit(roman_numeral, num, my_digit_hash, -3)
+    calculate_digit(roman_numeral, num, -3)
   end
 
   def thousands_digit (roman_numeral, num)
-    my_digit_hash = {1 => 'M', 2 => 'MM', 3 => 'MMM', 4 => '', 5 => '', 6 => '', 7 => '', 8 => '', 9 => ''}
-    calculate_digit(roman_numeral, num, my_digit_hash, -4)
+    calculate_digit(roman_numeral, num, -4)
   end
 
-  def calculate_digit (roman_numeral, num, my_digit_hash, my_index)
+  def calculate_digit (roman_numeral, num, my_index)
+    roman_numeral_conversion_hash = {
+      -1 => {0 => '', 1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV', 5 => 'V', 6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX'},
+      -2 => {1 => 'X', 2 => 'XX', 3 => 'XXX', 4 => 'XL', 5 => 'L', 6 => 'LX', 7 => 'LXX', 8 => 'LXXX', 9 => 'XC'},
+      -3 => {1 => 'C', 2 => 'CC', 3 => 'CCC', 4 => 'CD', 5 => 'D', 6 => 'DC', 7 => 'DCC', 8 => 'DCCC', 9 => 'CM'},
+      -4 => {1 => 'M', 2 => 'MM', 3 => 'MMM', 4 => '', 5 => '', 6 => '', 7 => '', 8 => '', 9 => ''}
+    }
     digit = num.to_s.split('')[my_index].to_i
-    roman_numeral = "#{roman_numeral}#{my_digit_hash[digit]}"
+    roman_numeral = "#{roman_numeral}#{roman_numeral_conversion_hash[my_index][digit]}"
   end
 end
